1334f2e09c250694c83cf3e67c98d8925bb119fd
Ian Koller <ikoller47@gmail.com>
2014-02-17 15:59:10 -0600

    Adding Version Control

---
+++ getQuestions.js
@@ -0,0 +1,7 @@
+function getQuestions() {
+	question1={text:"Who was the first president of the United States?",a:"George Washington",b:"Abraham Lincoln", c:"Barack Obama", d:"Nancy Pelosi", answer:"A"};
+    question2={text:"Who is the creator of this game?",a:"Your mom",b:"Ian Koller", c:"Obama", d:"Bill Gates", answer:"B"};
+    question3={text:"What are you playing this on?",a:"Chromecast",b:"Nintendo 64", c:"Obama", d:"A Toaster", answer:"A"};
+    gameQuestions = [question1, question2, question3];
+    return gameQuestions;
+}
